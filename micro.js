89dec9c6f56acee86c98eeb0739bef4b2437220c
fehbr800 <matheusemanoel1407@gmail.com>
2022-02-25 19:19:02 -0300

    microondas

---
+++ micro.js
@@ -0,0 +1,91 @@
+
+//MATHEUS EMANOEL GRUPO:11
+
+console.log(`
+
+1 - Pipoca – 10 segundos (padrão);
+2 - Macarrão – 8 segundos (padrão);
+3 - Carne – 15 segundos (padrão);
+4 - Feijão – 12 segundos (padrão);
+5 - Brigadeiro – 8 segundos (padrão); 
+
+    ESCOLHA SEU PRATO!!
+
+`)
+
+
+
+let prato =  "feijão:"
+let tempo = "12s"
+console.log(prato,tempo)
+
+switch(tempo){
+
+    //PIPOCA
+
+        case 1, "10s":
+            console.log( "Prato pronto, bom apetite!!!");
+            break
+        case 2, "20s":
+            console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
+            break
+        case 3, "30s":
+            console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
+            break
+    
+   //MACARRÃO
+        case 4, "8s":
+              console.log( "Prato pronto, bom apetite!!!");
+                break
+        case 5, "16s":
+             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
+                break
+        case 6, "24s":
+             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
+                break
+
+    //CARNE
+        case 7, "15s":
+              console.log( "Prato pronto, bom apetite!!!");
+                break
+        case 8, "30s":
+             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
+                break
+        case 9, "45s":
+             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
+                break
+
+
+       //FEIJÃO   
+
+        case 10, "12s":
+            console.log( "Prato pronto, bom apetite!!!");
+                    break
+        case 11, "24s":
+              console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
+                    break
+        case 12, "36s":
+             console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
+                    break
+
+    //BRIGADEIRO
+        case 13, "8s":
+             console.log( "Prato pronto, bom apetite!!!");
+                        break
+         case 14, "16s":
+             console.log ("Comida queimou!!",  "Prato pronto, bom apetite!!!");
+                        break
+         case 15, "24s":
+              console.log("Kbuuumm!!", "Prato pronto, bom apetite!!!")
+                        break
+                        
+
+        default:(
+            console.log("Tempo insuficiente!!", "Prato pronto, bom apetite!!!")
+    )    
+
+        
+}
+
+
+
